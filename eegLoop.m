8a0f1bce40bacca17598444f5c1067c05e947d9e
Raj Aiyer (iMAC) <sriraj.aiyer@psy.ox.ac.uk>
2020-02-13 18:14:04 +0000

    EEG loop working and trial breakdown

--- eegLoop.m
+++ eegLoop.m
@@ -22,17 +22,16 @@ for t = 1 : length(trials)
     tilesCoord(nextToFlip,3) = 1;
     Screen('FillRect',Sc.window,vars.colourCodeN,squareCoords(:,nextToFlip)');
     vars = drawGrid(Sc.window,vars);
-    drawAnswerBox(Sc.window,answerCoords);
     Screen('Flip',Sc.window);
     % WHEN FLIP OCCURS
     while (flipEndFlag == 0)
         [x,y,buttons] = GetMouse;
         % If left mouse button is clicked
-        if(buttons(1)||buttons(2)||buttons(3))
+        if(buttons(1))
             while 1
                 % Wait for mouse release.
                 [x,y,buttons] = GetMouse; 
-                if(~(buttons(1))&&~(buttons(2))&&~(buttons(3)))
+                if(~(buttons(1)))
                     flipTimestamps = [flipTimestamps GetSecs];
                     numOfFlips = numOfFlips + 1;
                     decPoints = decPoints - vars.decreasingDec;
@@ -53,9 +52,48 @@ for t = 1 : length(trials)
                         nextToFlip = ceil(rand*25);
                     end
                     tilesCoord(nextToFlip,3) = 1;
+                    
+                    trials(t).trialBreakdown(numOfFlips).flipNumber = numOfFlips;
+                    trials(t).trialBreakdown(numOfFlips).colourRevealed = vars.colourNames(grid(arrayX,arrayY));
+                    reducedGrid = nonzeros(hiddenGrid);
+                    trials(t).trialBreakdown(numOfFlips).majorityAmount = sum(reducedGrid==mode(reducedGrid))-sum(reducedGrid~=mode(reducedGrid));
+                    if (trials(t).trialBreakdown(numOfFlips).majorityAmount == 0)
+                        trials(t).trialBreakdown(numOfFlips).majorityColour = 'neither';
+                    else
+                        trials(t).trialBreakdown(numOfFlips).majorityColour = vars.colourNames(mode(reducedGrid));
+                    end
+                    trials(t).trialBreakdown(numOfFlips).majPCorrect = getPcorrect(mode(reducedGrid),reducedGrid,vars);
+                    trials(t).trialBreakdown(numOfFlips).timestamp = flipTimestamps(numOfFlips);
+                    if numOfFlips == 1
+                        trials(t).trialBreakdown(numOfFlips).timeSinceLastFlip = flipTimestamps(1) - trials(t).trialStart;
+                    else
+                        trials(t).trialBreakdown(numOfFlips).timeSinceLastFlip = flipTimestamps(numOfFlips) - flipTimestamps(numOfFlips-1);
+                    end
+                    trials(t).trialBreakdown(numOfFlips).currentGrid = hiddenGrid;
+                    trials(t).trialBreakdown(numOfFlips).tileClicked = [arrayX arrayY];
+                    
                     Screen('FillRect',Sc.window,vars.colourCodeN,squareCoords(:,nextToFlip)');
-                    drawAnswerBox(Sc.window,answerCoords);
                     Screen('Flip',Sc.window);
+                    break;
+                end
+            end
+        end
+        if((buttons(2)||buttons(3))&&numOfFlips>0)
+            while 1
+                % Wait for mouse release.
+                [x,y,buttons] = GetMouse; 
+                if(~buttons(2)&&~buttons(3))
+                    trials(t).finalGridState = hiddenGrid;
+                    hiddenGrid = nonzeros(hiddenGrid);
+                    trials(t).numOfTilesRevealed = numOfFlips;
+                    trials(t).majorityMargin = sum(hiddenGrid==mode(hiddenGrid))-sum(hiddenGrid~=mode(hiddenGrid));
+                    if (trials(t).majorityMargin == 0)
+                        trials(t).majorityRevealed = 'neither';
+                    else
+                        trials(t).majorityRevealed = vars.colourNames(mode(hiddenGrid));
+                    end
+                    trials(t).finalPCorrect = getPcorrect(mode(hiddenGrid),trials(t).finalGridState,vars);
+                    flipEndFlag = 1;
                     break;
                 end
             end
@@ -65,7 +103,6 @@ for t = 1 : length(trials)
     vars = drawGrid(Sc.window,vars);
     drawOptions(Sc.window,colours(1,:),colours(2,:),optionCoords)
     Screen('Flip',Sc.window);
-    
     while (answerFlag == 0)
         [x,y,buttons] = GetMouse;
         % If mouse button is clicked
@@ -73,7 +110,56 @@ for t = 1 : length(trials)
             while 1
                 % Wait for mouse release.
                 [x,y,buttons] = GetMouse; 
-                if(~(buttons(1)))  
+                if(~(buttons(1)))
+                    trials(t).finalAnswerTime = GetSecs;
+                    trials(t).finalAns = 1;
+                    trials(t).finalColour = vars.colourNames(1);
+                    if (trials(t).trueAns == 1)
+                        trials(t).correct = 1;
+                        trialText = "correct!";
+                        if (trials(t).type ~= 'decreasing')
+                            points = points + vars.fixedPointsWin;
+                            trials(t).reward = vars.fixedPointsWin;
+                        else
+                            points = points + decPoints;
+                            trials(t).reward = decPoints;
+                        end
+                    else
+                        trials(t).correct = 0;
+                        trialText = "incorrect!";
+                        Beeper(1000,.4,.5);
+                        points = points - vars.wrongTokenLoss;
+                        trials(t).reward = vars.wrongTokenLoss*-1;
+                    end
+                    answerFlag = 1;
+                    break;
+                end
+            end
+        elseif (buttons(2)||buttons(3))
+            while 1
+                % Wait for mouse release.
+                [x,y,buttons] = GetMouse; 
+                if(~(buttons(2))&&~(buttons(3)))
+                    trials(t).finalAnswerTime = GetSecs;
+                    trials(t).finalAns = 2;
+                    trials(t).finalColour = vars.colourNames(2);
+                    if (trials(t).trueAns == 2)
+                        trials(t).correct = 1;
+                        trialText = "correct!";
+                        if (trials(t).type ~= 'decreasing')
+                            points = points + vars.fixedPointsWin;
+                            trials(t).reward = vars.fixedPointsWin;
+                        else
+                            points = points + decPoints;
+                            trials(t).reward = decPoints;
+                        end
+                    else
+                        trials(t).correct = 0;
+                        trialText = "incorrect!";
+                        points = points - vars.wrongTokenLoss;
+                        trials(t).reward = vars.wrongTokenLoss*-1;
+                    end
+                    answerFlag = 1;
                     break;
                 end
             end
