9783b7f20aa3a6ad601e685a2bcd3a9acc23fce2
Jasmine Tan <jasmine.tan6032@gmail.com>
2021-10-12 00:28:56 +0100

    Altered code for answer and flip tile
    Flip tile is now on the right and answer is using the left hand

--- eegOnscreenInstruct.m
+++ eegOnscreenInstruct.m
@@ -7,6 +7,6 @@ function eegOnscreenInstruct(win,vars)
     optionCoords = vars.optionCoords;
     Screen('FrameRect', win, [0,0,0], optionCoords(:,1));
     Screen('FrameRect', win, [0,0,0], optionCoords(:,2));
-    Screen('DrawText', win, 'Flip Tile', (optionCoords(1,1)+optionCoords(3,1))/2, (optionCoords(2,1)+optionCoords(4,1))/2, [255,255,255]);
-    Screen('DrawText', win, 'Answer', (optionCoords(1,2)+optionCoords(3,2))/2, (optionCoords(2,2)+optionCoords(4,2))/2, [255,255,255]);
+    Screen('DrawText', win, 'Answer', (optionCoords(1,1)+optionCoords(3,1))/2, (optionCoords(2,1)+optionCoords(4,1))/2, [255,255,255]);
+    Screen('DrawText', win, 'Flip Tile', (optionCoords(1,2)+optionCoords(3,2))/2, (optionCoords(2,2)+optionCoords(4,2))/2, [255,255,255]);
 end
